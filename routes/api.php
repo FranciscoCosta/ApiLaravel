665ab6b865b981f0a72a33a8ebb09a56026a91f5
ico <francisco100eg@gmail.com>
2023-10-16 13:40:35 -0300

    Fix: Routes

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,12 @@
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::patch('/users/{id}', [UserController::class, 'update']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
+Route::apiResource('users', UserController::class);
+// Route::patch('/users/{id}', [UserController::class, 'update']);
+// Route::get('/users/{id}', [UserController::class, 'show']);
+// Route::get('/users', [UserController::class, 'index']);
+// Route::post('/users', [UserController::class, 'store']);
+// Route::delete('/users/{id}', [UserController::class, 'destroy']);
 
 Route::get('/', function(){
     return response()->json(['message' => 'Hello World!'], 200);
