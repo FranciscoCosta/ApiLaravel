8c6d7abc348a1d4cfe1840384f687f0f22b85414
ico <francisco100eg@gmail.com>
2023-10-16 10:08:10 -0300

    Feat: Runned migration

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
-        return [];
+        return User::all();
     }
 }
