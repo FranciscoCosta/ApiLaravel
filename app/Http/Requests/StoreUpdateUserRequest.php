75f6ac59bd67f4b4f4aa7fd50434e183fe88a63e
ico <francisco100eg@gmail.com>
2023-10-16 10:56:44 -0300

    Feat: Created validation for store and update

--- app/Http/Requests/StoreUpdateUserRequest.php
+++ app/Http/Requests/StoreUpdateUserRequest.php
@@ -11,7 +11,7 @@ class StoreUpdateUserRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,17 @@ class StoreUpdateUserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|min:3|max:255',
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'min:3',
+                'max:255',
+                'unique:users',
+            ],
+            'password' => 'required|string|min:6|max:16',
         ];
+
     }
 }
